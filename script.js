b1631dd219af13acecc92d717833ce10f88e16e4
MaeFire <masked_phoenix@yahoo.com>
2022-11-04 01:41:45 -0500

    Fix memory leak and add reset functionality
    I fixed the leak that wouuld spawn endless grid cells, and now the reset
    button refreshes window on click.

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-//grid related
-const wrapper = document.getElementById("wrapper");
-//slider related
+let gridReset        = false;
+const wrapper        = document.getElementById("wrapper");
 const sliderSizeText = document.getElementById("sliderText");
 const gridSlider     = document.getElementById("gridSlider");
+const resetBtn       = document.getElementById("resetButton");
 
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~MENU RELATED~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+resetBtn.addEventListener('click',() =>{
+    window.location.reload();
+});
 
 gridSlider.onmousemove = function(e) {updateSliderText(e.target.value);}
-gridSlider.onmouseup = function(e) {grid(e.target.value);}
-      
+
 function updateSliderText(size) {
     sliderSizeText.innerText = `${size}x${size}`;
 }
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~GRID RELATED~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+gridSlider.addEventListener('pointerup', () => {
+  wrapper.textContent=""
+  if(wrapper.textContent == ""){
+    gridSlider.onmouseup = function(e){grid(e.target.value);}
+  }
+});
 function grid(cells){
     wrapper.style.gridTemplateColumns = `repeat(${cells}, 1fr)`; 
     wrapper.style.gridTemplateRows = `repeat(${cells}, 1fr)`;
     for(let i=0; i<(cells * cells);i++){
         const gridCell = document.createElement('div');
         gridCell.classList.add("gridSquare");
-        wrapper.appendChild(gridCell);    
+        wrapper.appendChild(gridCell);
     }
 }
+
+do{
+    grid(16);
+    gridReset = false;
+} while (gridReset === true);
+
+//TODO Add function to buttons. Reset button. Find a way to lock grid to single click?
+//new cells made, but not shown on each slider click.

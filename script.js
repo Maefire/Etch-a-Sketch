aaaebad5d0e035fe36ee49c392fa82b066bbada3
MaeFire <masked_phoenix@yahoo.com>
2022-11-09 01:08:08 -0600

    Add toggle to draw space
    Draw space was always on, and could become hard to draw what you wanted.
    
    Implemented an "onclick" listener so that it would turn on or off,
    depending on a boolean value.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let gridReset        = false;
+let toggleState      = "";
 const wrapper        = document.getElementById("wrapper");
+const resetBtn       = document.getElementById("resetButton");
 const sliderSizeText = document.getElementById("sliderText");
 const gridSlider     = document.getElementById("gridSlider");
-const resetBtn       = document.getElementById("resetButton");
+const currentColor   = document.getElementsByClassName("penColor");
+//TODO let penColor = e.target.value? Have pen color update on click?
 
 
 
@@ -10,7 +13,7 @@ const resetBtn       = document.getElementById("resetButton");
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ~~~~~~~~~~~~~~~~~~MENU RELATED~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-resetBtn.addEventListener('click',() =>{
+resetBtn.addEventListener("click",() =>{
     window.location.reload();
 });
 
@@ -25,26 +28,47 @@ function updateSliderText(size) {
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ~~~~~~~~~~~~~~~~~~GRID RELATED~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-gridSlider.addEventListener('pointerup', () => {
+gridSlider.addEventListener("pointerup", () => {
   wrapper.textContent=""
   if(wrapper.textContent == ""){
     gridSlider.onmouseup = function(e){grid(e.target.value);}
   }
 });
-function grid(cells){
-    wrapper.style.gridTemplateColumns = `repeat(${cells}, 1fr)`; 
-    wrapper.style.gridTemplateRows = `repeat(${cells}, 1fr)`;
-    for(let i=0; i<(cells * cells);i++){
-        const gridCell = document.createElement('div');
-        gridCell.classList.add("gridSquare");
+
+function grid(cellSize){
+    wrapper.style.gridTemplateColumns = `repeat(${cellSize}, 1fr)`; 
+    wrapper.style.gridTemplateRows = `repeat(${cellSize}, 1fr)`;
+    for(let i=0; i<(cellSize * cellSize);i++){
+        const gridCell = document.createElement("div");
+        gridCell.classList.add("cell");
         wrapper.appendChild(gridCell);
     }
 }
 
+
+wrapper.onmouseover = (e) =>{
+    wrapper.onmouseup = (e) =>{
+        if(toggleState !== true){
+            toggleState = true;
+            return;
+        }else{
+            toggleState = false;
+        }
+    }
+    if(toggleState === true){
+        if(e.target && e.target.matches("div.cell")){
+            // console.log(e.target)
+            e.target.classList.add("penColor");
+          }
+    }    
+}
 do{
     grid(16);
     gridReset = false;
 } while (gridReset === true);
 
-//TODO Add function to buttons. Reset button. Find a way to lock grid to single click?
-//new cells made, but not shown on each slider click.
+//TODO Add function to buttons. Reset button. 
+/* toggle: add current color for selection (let currentColor = [])
+then add a class of "draw" on toggle to all grid divs 
+if class of draw exists, mouseover event fires.
+on toggle/click remove class of draw, disabling mouseover event from firing*/
